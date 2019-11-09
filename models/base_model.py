f69b5af833e50f711b7629cf5e0c91fc6745e27f
miguel-dev <miguelacortes@zoho.com>
2019-11-09 17:44:09 +0000

    [Fixes] Improved efficiency for task 4

--- models/base_model.py
+++ models/base_model.py
@@ -22,11 +22,11 @@ class BaseModel():
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
                 if key == "created_at" or key == "update_at":
                     setattr(self, key,
                             datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
