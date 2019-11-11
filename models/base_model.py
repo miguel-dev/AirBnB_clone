1c455569f6241c509979355741d878e9a3881a7e
miguel-dev <miguelacortes@zoho.com>
2019-11-11 21:54:11 +0000

    [Feature] Task 5. Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 """
 from datetime import datetime, date
 import uuid
+from models import storage
 
 
 class BaseModel():
@@ -31,6 +32,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -45,6 +47,7 @@ class BaseModel():
            with the current_time
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
